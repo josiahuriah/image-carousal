9fb9534db078250270ff3c0ec2d5b0f63b4c63fa
Josh <josh.duncanson@gmail.com>
2023-08-18 07:23:29 -0400

    first style changes

--- image-carousal/src/App.js
+++ image-carousal/src/App.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+import './App.css';
+//repository of images to use in carousal
 import imagez from './images.js';
 
+
+//carousal definition
 export default function Gallery() {
     const [index, setIndex] = useState(0);
 
+    // changes the index for image selection by either incrementing or resetting
     function handleClick() {
       if (index >= imagez.length-1){
         setIndex(0)
@@ -12,15 +17,18 @@ export default function Gallery() {
       }
     }
 
+    // sets current image based on index 
     let currentImage = imagez[index];
     console.log(currentImage)
 
     return(
-        <div>
+        <div className='container'>
+            <h1 className='heading'>The Great Carousal</h1>
+            <img className='image' src={currentImage}></img>
             <button
             onClick = {handleClick}
+            className='button'
             >Next Image</button>
-            <img src={currentImage}></img>
         </div>
     )
 }
