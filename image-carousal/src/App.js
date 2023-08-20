39e9fbbf8082a059bafc1211d55a188511812fd8
Josh <josh.duncanson@gmail.com>
2023-08-20 12:08:35 -0400

    Added a prev button, styles and made code more concise

--- image-carousal/src/App.js
+++ image-carousal/src/App.js
@@ -9,13 +9,10 @@ export default function Gallery() {
     const [index, setIndex] = useState(0);
 
     // changes the index for image selection by either incrementing or resetting
-    function handleClick() {
-      if (index >= imagez.length-1){
-        setIndex(0)
-      } else{
-        setIndex( index + 1);
-      }
-    }
+    const handleNextImage = () => {(index >= imagez.length-1) ? setIndex(0) : setIndex(index + 1)};
+
+    //changes the index for image selection by either decrementing or setting to end of image list
+    const handlePrevImage = () => {(index===0) ? setIndex(imagez.length - 1) : setIndex(index - 1)};
 
     // sets current image based on index 
     let currentImage = imagez[index];
@@ -24,12 +21,18 @@ export default function Gallery() {
     //
     return(
         <div className='container'>
-            <h1 className='heading'>The Great Carousal</h1>
-            <img className='image' src={currentImage}></img>
+            <h1 className='heading'>The Great Carousal</h1><br></br>
+            <span>
             <button
-            onClick = {handleClick}
-            className='button'
+            onClick = {handleNextImage}
+            className='prev-button button'
+            >Previous Image</button>
+            <img className='image' alt={currentImage} src={currentImage}></img>
+            <button
+            onClick = {handlePrevImage}
+            className='next-button button'
             >Next Image</button>
+            </span>
         </div>
     )
 }
